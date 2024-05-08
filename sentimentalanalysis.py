4681fe9e4ebd186e3baadb0d61a32ee66893a823
abeeranajam31 <abeeranajam@gmail.com>
2024-05-08 12:12:16 +0500

    Building model using Naive Bayes Classifier

--- sentimentalanalysis.py
+++ sentimentalanalysis.py
@@ -50,3 +50,65 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features=1500)
 X = cv.fit_transform(corpus).toarray()
 y = df.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+     
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import MultinomialNB
+classifier = MultinomialNB()
+classifier.fit(X_train, y_train)
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# Accuracy, Precision and Recall
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+score1 = accuracy_score(y_test,y_pred)
+score2 = precision_score(y_test,y_pred)
+score3= recall_score(y_test,y_pred)
+print("---- Scores ----")
+print("Accuracy score is: {}%".format(round(score1*100,2)))
+print("Precision score is: {}".format(round(score2,2)))
+print("Recall score is: {}".format(round(score3,2)))
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+cm
+     
+# Plotting the confusion matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+plt.figure(figsize = (10,6))
+sns.heatmap(cm, annot=True, cmap="YlGnBu", xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+plt.xlabel('Predicted values')
+plt.ylabel('Actual values')
+     
+# Hyperparameter tuning the Naive Bayes Classifier
+best_accuracy = 0.0
+alpha_val = 0.0
+for i in np.arange(0.1,1.1,0.1):
+  temp_classifier = MultinomialNB(alpha=i)
+  temp_classifier.fit(X_train, y_train)
+  temp_y_pred = temp_classifier.predict(X_test)
+  score = accuracy_score(y_test, temp_y_pred)
+  print("Accuracy score for alpha={} is: {}%".format(round(i,1), round(score*100,2)))
+  if score>best_accuracy:
+    best_accuracy = score
+    alpha_val = i
+print('--------------------------------------------')
+print('The best accuracy is {}% with alpha value as {}'.format(round(best_accuracy*100, 2), round(alpha_val,1)))
+
+
+classifier = MultinomialNB(alpha=0.2)
+classifier.fit(X_train, y_train)
